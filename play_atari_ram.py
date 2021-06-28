116ac94f85bf664af806542ddbbe85cd68067139
Kristian <kristian.jarvenpaa@vakuutio.fi>
2021-06-28 18:04:07 +0300

    update

--- play_atari_ram.py
+++ play_atari_ram.py
@@ -1,8 +1,30 @@
 from cgp import *
 import gym
 import time
-from atari_ram import play_atari_ram
+from statistics import mean
+# from atari_ram import play_atari_ram
 
+def play_atari_ram(game, ind):
+  env = gym.make(game)
+  reward_list = []
+  for i_episode in range(10):
+    observation = env.reset()
+    done = False
+    rewards = 0
+    while not done:
+      # env.render()
+      outputs = ind(observation)
+      action = outputs.index(max(outputs))
+
+      observation, reward, done, info = env.step(action)
+      rewards = rewards + reward 
+      if done:
+        # print("Episode finished with {} points".format(rewards))
+        reward_list.append(rewards)
+        break
+  print(mean(reward_list),'&', max(reward_list), '&' ,min(reward_list))
+  print('\n-----------------\n')
+  env.close()
 
 n_inputs = 128
 n_outputs =3 
@@ -12,11 +34,21 @@ KERN = ['sum', 'diff', 'mul', 'pdiv', 'sin','cos', 'log', 'exp', 'psqrt', 'sig',
 kernels = kernel_set_double(KERN)()
 
 games = [['Assault-ram-v0',7], ['Bowling-ram-v0',6], ['Boxing-ram-v0',18], ['Pong-ram-v0',6], ['KungFuMaster-ram-v0', 14]]
+ends = ['_simple', '_only_active', '_5_rounds']
 
-game = games[0][0]
-n_outputs = games[0][1]
+for game_tuple in games:
+  for end in ends:
+    game = game_tuple[0]
+    n_outputs = game_tuple[1]
 
-ind = generate_individual(n_inputs, n_outputs, n_cols, arity, kernels)
-load_genes(ind, 'ind/all_time_Assault-ram-v0_simple')
+    ind = generate_individual(n_inputs, n_outputs, n_cols, arity, kernels)
 
-play_atari_ram(game, ind)
+    print('Game:', game, 'Version:', end)
+
+    print('all_time')
+    load_genes(ind, 'ind/all_time_'+game+end)
+    play_atari_ram(game, ind)
+
+    print('last one')
+    load_genes(ind, 'ind/'+game+end)
+    play_atari_ram(game, ind)
